c422d0a1a857e80b09c5784e215249f3571dfdad
Lucas Silvestre <lucas_silvestre.10@hotmail.com>
2022-08-24 20:32:36 -0300

    desafio 9 feito

--- src/challenges.js
+++ src/challenges.js
@@ -84,11 +84,13 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(palavra) {
+  let result = palavra.replace(/a/g, '1').replace(/e/g, '2').replace(/i/g, '3').replace(/o/g, '4').replace(/u/g, '5');
+   return result;
 }
-function decode() {
-  // seu código aqui
+function decode(palavra) {
+  let result = palavra.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+  return result;
 }
 
 // Desafio 10
